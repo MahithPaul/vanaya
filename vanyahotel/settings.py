f3c580f06cbff825911186fcf56fde9aee17ddfa
Dharmapuri Mahith Paul <mahithpaul07@gmail.com>
2023-06-18 00:32:48 +0530

    initial  upload

--- vanyahotel/settings.py
+++ vanyahotel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a_ad_2nag0r6jhkxlp8%_0p_%p5l+ru34e&e(vlm()%gh=#o(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'vanyahotel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
